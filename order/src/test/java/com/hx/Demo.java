4e131b1f36ebcd126826fc125761b12a39360bd8
hx <1375783560@qq.com>
2025-08-05 17:35:54 +0800

    同步

--- order/src/test/java/com/hx/Demo.java
+++ order/src/test/java/com/hx/Demo.java
@@ -1,16 +1,10 @@
 package com.hx;
 
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-
 public class Demo {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
 }
