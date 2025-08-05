d01b2d5d6368f299e53fbff2bd0500c92adaa512
hx <1375783560@qq.com>
2025-08-05 17:12:20 +0800

    整合

--- order/src/test/java/com/hx/Demo.java
+++ order/src/test/java/com/hx/Demo.java
@@ -1,31 +1,16 @@
 package com.hx;
 
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class Demo {
     public static void main(String[] args) {
-        AtomicBoolean connectionClosed = new AtomicBoolean(false);
-        while (!connectionClosed.get()) {
-            send("内容", connectionClosed);
-        }
-        if (connectionClosed.get()) {
-            System.out.println("连接已关闭");
-            return;
-        }
-        System.out.println("继续逻辑");
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.toString());
     }
 
-    private static void send(String text, AtomicBoolean connectionClosed) {
-        try {
-            System.out.println(text);
-            throw new RuntimeException();
-        } catch (Exception e) {
-            if (!connectionClosed.getAndSet(true)) {
-                System.out.println("客户端已断开连接");
-            }
-            System.out.println("执行关闭逻辑");
-        }
-    }
 }
